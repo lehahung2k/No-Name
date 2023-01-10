fda18b16f4a509b8e73bc476c663670bc372d330
lehahung2k <hung2000yt@gmail.com>
2023-01-10 22:03:20 +0700

    update code

--- src/components/FileReader/index.js
+++ src/components/FileReader/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import textImg from '../../assets/image.txt'
-import {useState} from "react";
+import textImg2 from '../../assets/file2.txt'
+import { useState } from "react";
 import TextareaAutosize from 'react-textarea-autosize';
 
 const herDoB = '2003-11-22';
@@ -29,17 +30,25 @@ export default function TextFileReader() {
     }
     const processData = () => {
         fetch(textImg)
-        .then(r => r.text())
-        .then(text => {
-            setFileData(text)
-        });
+            .then(r => r.text())
+            .then(text => {
+                setFileData(text)
+            });
     }
 
-    return(
+    const handleChange = (event) => {
+        fetch(textImg2)
+            .then(r => r.text())
+            .then(text => {
+                setFileData(text);
+            });
+    }
+
+    return (
         <div className="check-bar">
             <label>Enter your name!</label>
             <p>
-                <input className='style' type="text" value={nameInput} onChange={(data) => {setNameInput(data.target.value)}}/>
+                <input className='style' type="text" value={nameInput} onChange={(data) => { setNameInput(data.target.value) }} />
                 <button onClick={showNameInput} className='style'>Submit</button>
             </p>
             {isShowName &&
@@ -47,13 +56,13 @@ export default function TextFileReader() {
                     <p>Hi, {nameInput}!</p>
                     <label>Enter your birthday: </label>
                     <p>
-                        <input className='style' type="date" value={birthday} onChange={(data) => {setBirthday(data.target.value)}}/>
+                        <input className='style' type="date" value={birthday} onChange={(data) => { setBirthday(data.target.value) }} />
                         <button className='style' type="submit" onClick={checkBirthday}> Let's see!</button>
                     </p>
                 </div>
             }
             {isShowImg &&
-                <TextareaAutosize className="output" value={fileData} onChange={processData}/>
+                <TextareaAutosize className="output" value={fileData} onChange={processData} onKeyDown={handleChange}/>
             }
         </div>
     );
